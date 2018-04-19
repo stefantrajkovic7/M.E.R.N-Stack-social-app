ce035e56deda990cef60abbb6b51e4cb1edaa405
saibot777 <stefantrajkovic777@gmail.com>
2018-04-19 14:52:41 +0200

    code structure update

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// Express Configuration
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// API Routes
+require('./routes')(app);
 
 // Connect to MongoDB
 mongoose
     .connect(db)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
-
-app.get('/', (req, res) => res.send('Hello'));
 
 module.exports = app;
