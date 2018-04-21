e9dd49103bae13f7d626a1f8c0601a02f8cc828f
saibot777 <stefantrajkovic777@gmail.com>
2018-04-21 22:03:32 +0200

    refactored to more dry principals

--- api/v1/profiles/controller.js
+++ api/v1/profiles/controller.js
@@ -51,26 +51,29 @@ exports.create = (req, res) => {
     const profileFields = {};
     profileFields.user = req.user.id;
 
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = req.body.company;
-    if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bio) profileFields.bio = req.body.bio;
-    if (req.body.status) profileFields.status = req.body.status;
-    if (req.body.githubusername)
-        profileFields.githubusername = req.body.githubusername;
+    const fields = [
+        "handle",
+        "company",
+        "website",
+        "location",
+        "bio",
+        "status",
+        "githubprofile",
+        "youtube",
+        "twitter",
+        "facebook",
+        "linkedin",
+        "instagram"
+    ];
+
+    fields.forEach(field => {
+        if (req.body[field]) profileFields[field] = req.body[field];
+    });
+
     // Skills - Split into array
     if (typeof req.body.skills !== 'undefined') {
         profileFields.skills = req.body.skills.split(',');
     }
-
-    // Social
-    profileFields.social = {};
-    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
-    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
-    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
-    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
-    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
     Profile.findOne({ user: req.user.id }).then(profile => {
         if (profile) {
