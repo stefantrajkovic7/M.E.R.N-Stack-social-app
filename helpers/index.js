19f6657e1fc3437e9647e7c6ce354cc310c626ae
saibot777 <stefantrajkovic777@gmail.com>
2018-04-21 22:20:49 +0200

    refactored to more dry principals part II

--- helpers/index.js
+++ helpers/index.js
@@ -3,33 +3,21 @@ const Validator = require('validator');
 exports.validateRegistration = data => {
     let errors = {};
 
-    data.name = !isEmpty(data.name) ? data.name : '';
-    data.email = !isEmpty(data.email) ? data.email : '';
-    data.password = !isEmpty(data.password) ? data.password : '';
-    data.password2 = !isEmpty(data.password2) ? data.password2 : '';
+    dataFields = ["name", "email", "password", "password2"];
 
-    if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-        errors.name = 'Name must be between 2 and 30 characters';
-    }
-
-    if (Validator.isEmpty(data.name)) {
-        errors.name = 'Name field is required';
-    }
+    dataFields.forEach(field => {
+        data[field] = !isEmpty(data[field]) ? data[field] : "";
+        if (Validator.isEmpty(data[field])) {
+            errors[field] = `${field} field is required`;
+        }
+    });
 
     if (!Validator.isEmail(data.email)) {
         errors.email = 'Email is invalid';
     }
 
-    if (Validator.isEmpty(data.email)) {
-        errors.email = 'Email field is required';
-    }
-
-    if (Validator.isEmpty(data.password)) {
-        errors.password = 'Password field is required';
-    }
-
-    if (Validator.isEmpty(data.password2)) {
-        errors.password2 = 'Password Confirm field is required';
+    if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+        errors.name = 'Name must be between 2 and 30 characters';
     }
 
     if (!Validator.isLength(data.password, {min: 6})) {
@@ -50,19 +38,17 @@ exports.validateRegistration = data => {
 exports.validateLogin = data => {
     let errors = {};
 
-    data.email = !isEmpty(data.email) ? data.email : '';
-    data.password = !isEmpty(data.password) ? data.password : '';
+    dataFields = ["email", "password"];
+
+    dataFields.forEach(field => {
+        data[field] = !isEmpty(data[field]) ? data[field] : "";
+        if (Validator.isEmpty(data[field])) {
+            errors[field] = `${field} field is required`;
+        }
+    });
 
     if (!Validator.isEmail(data.email)) {
         errors.email = 'Email is invalid';
-    }
-
-    if (Validator.isEmpty(data.email)) {
-        errors.email = 'Email field is required';
-    }
-
-    if (Validator.isEmpty(data.password)) {
-        errors.password = 'Password field is required';
     }
 
     return {
