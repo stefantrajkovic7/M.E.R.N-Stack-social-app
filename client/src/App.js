701b9af7c0657dd320f4b9b20f70df5b5c7e7f54
saibot777 <stefantrajkovic777@gmail.com>
2018-04-23 21:16:58 +0200

    layout UI component impl

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from "./components/layout";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My React App</h1>
+        <Layout/>
       </div>
     );
   }
