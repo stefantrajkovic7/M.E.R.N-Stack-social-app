7368d4228d2f52fb9b9b84ed8aa1f07360436089
saibot777 <stefantrajkovic777@gmail.com>
2018-04-23 21:49:29 +0200

    routing impl

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Layout from "./components/layout";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Layout/>
-      </div>
+        <Router>
+            <div className="App">
+                <Route exact path="/" component={Layout} />
+            </div>
+        </Router>
     );
   }
 }
