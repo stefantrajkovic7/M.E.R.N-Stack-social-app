f4c5e22b342596f66b458cc7436285bc144530b3
saibot777 <stefantrajkovic777@gmail.com>
2018-04-24 10:14:21 +0200

    login logic UI update

--- client/src/components/auth/login/Login.jsx
+++ client/src/components/auth/login/Login.jsx
@@ -1,10 +1,62 @@
 import React, { Component } from 'react';
 
 export class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        }
+    }
+
+    onChange = e => this.setState({
+        [e.target.name]: e.target.value
+    });
+
+    onSubmit = e => {
+        e.preventDefault();
+        const loginUser =  {
+            email: this.state.email,
+            password: this.state.password
+        };
+
+        console.log(loginUser)
+    };
+
     render() {
         return (
-            <div>
-                LOGIN
+            <div className="login">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Log In</h1>
+                            <p className="lead text-center">Sign in to your DevConnector account</p>
+                            <form onSubmit={this.onSubmit}>
+                                <div className="form-group">
+                                    <input
+                                        type="email"
+                                        className="form-control form-control-lg"
+                                        placeholder="Email Address"
+                                        value={this.state.email}
+                                        onChange={this.onChange}
+                                        name="email" />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="Password"
+                                        value={this.state.password}
+                                        onChange={this.onChange}
+                                        name="password" />
+                                </div>
+                                <input
+                                    type="submit" className="btn btn-info btn-block mt-4" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
