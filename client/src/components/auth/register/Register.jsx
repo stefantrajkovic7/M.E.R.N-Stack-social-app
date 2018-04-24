d17ed9c61109774fc2b093fa7a5f0b4579e003a5
saibot777 <stefantrajkovic777@gmail.com>
2018-04-24 09:48:44 +0200

    UI polish update

--- client/src/components/auth/register/Register.jsx
+++ client/src/components/auth/register/Register.jsx
@@ -3,8 +3,31 @@ import React, { Component } from 'react';
 export class Register extends Component {
     render() {
         return (
-            <div>
-                REGISTER
+            <div class="register">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 m-auto">
+                            <h1 class="display-4 text-center">Sign Up</h1>
+                            <p class="lead text-center">Create your DevConnector account</p>
+                            <form action="create-profile.html">
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-lg" placeholder="Name" name="name" required />
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-lg" placeholder="Email Address" name="email" />
+                                    <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg" placeholder="Password" name="password" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg" placeholder="Confirm Password" name="password2" />
+                                </div>
+                                <input type="submit" class="btn btn-info btn-block mt-4" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
     return (
-        <footer className="bg-dark text-white mt-5 p-4 text-center">
+        <footer className="bg-dark text-white mt-5 p-4 text-center footer">
             Copyright &copy; 2018 Dev Connector <a rel="noopener noreferrer" className="footLink" target="_blank" href="https://github.com/saibot777"> Stefan Trajkovic</a>
         </footer>
     )
