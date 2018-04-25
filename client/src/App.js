85d1d11c12ef4c7370bece9703dc18eaf6718263
saibot777 <stefantrajkovic777@gmail.com>
2018-04-25 16:52:20 +0200

    RouteGuard/PrivateRoute for dahboard impl

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './App.css';
 import store from './store';
@@ -12,6 +12,7 @@ import Register from "./components/auth/register";
 import Login from "./components/auth/login";
 import setAuthToken from "./utils/setAuthToken";
 import {setCurrentUser, logoutUser, clearCurrentProfile} from "./store/selectors";
+import PrivateRoute from "./components/shared/PrivateRoute";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -45,7 +46,9 @@ class App extends Component {
                     <div className="container">
                         <Route exact path="/register" component={Register}/>
                         <Route exact path="/login" component={Login}/>
-                        <Route exact path="/dashboard" component={Dashboard}/>
+                        <Switch>
+                            <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+                        </Switch>
                     </div>
                     <Footer />
                 </div>
