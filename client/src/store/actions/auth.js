e636a063502d0fb1c4252b6dcfa0a39c6701f5d0
saibot777 <stefantrajkovic777@gmail.com>
2018-04-25 15:26:12 +0200

    actions refactored

--- client/src/store/actions/auth.js
+++ client/src/store/actions/auth.js
@@ -1,4 +1,2 @@
-import { createAction } from 'redux-actions';
-
-export const GET_ERRORS = createAction('[Auth] Register User Errors');
-export const SET_CURRENT_USER = createAction('[Auth] Register User ');
+export const GET_ERRORS = '[Auth] Register User Errors';
+export const SET_CURRENT_USER = '[Auth] Register User ';
