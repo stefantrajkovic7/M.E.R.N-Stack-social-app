e25a6c8bdfaa3d9d96bf112d1cce286a952e532d
saibot777 <stefantrajkovic777@gmail.com>
2018-05-14 16:22:33 +0200

    further deploy config...

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cors = require('cors');
+const path  = require('path');
 
 const app = express();
 
@@ -32,6 +33,16 @@ require('./services/passport')(passport);
 
 // API Routes
 require('./routes')(app);
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 // Connect to MongoDB
 mongoose
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    secret: process.env.SECRET_OR_KEY
+    secret: process.env.SECRET
 };
