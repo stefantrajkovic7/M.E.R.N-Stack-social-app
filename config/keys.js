c0e66a3305e0615dcec89eac8b6df5e8ddbc9918
saibot777 <stefantrajkovic777@gmail.com>
2018-05-14 15:55:43 +0200

    production build impl

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://admin:admin@ds147469.mlab.com:47469/social-network-mern',
-  secret: '53456435683456734865vhjfvhfdvhdfvghdf5r346754863485'
-};
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod')
+} else {
+    module.exports = require('./keys_dev')
+}
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
 const app = require('./app');
+const path  = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000;
 
